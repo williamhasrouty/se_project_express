aaa183b46debd83cb6a4e80e64abd40bd77af805
william.hasrouty <williamhasrouty@yahoo.com>
2025-11-09 14:12:03 -0800

    feat: set up dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const auth = require("./middlewares/auth");
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-const JWT_SECRET = "super-strong-secret"; // change later to process.env.JWT_SECRET
+const {JWT_SECRET = "super-strong-secret"} = process.env; 
 
 module.exports = { JWT_SECRET };
